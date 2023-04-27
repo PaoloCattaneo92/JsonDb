e5bdfd62d7f1400bfb1bcfb08b8d0dbb24e31f1d
Paolo Cattaneo <paolo.cattaneo@ubiquicom.com>
2023-04-27 18:00:59 +0200

    Solved file usage error and added delete function

---
+++ JsonDb.Example/Program.cs
@@ -0,0 +1,63 @@
+﻿using JsonDb;
+
+User FakeUser()
+{
+    return new User(
+        Faker.RandomNumber.Next(1000, 9999),
+        Faker.Name.FullName(),
+        Faker.Internet.Email()
+        );
+}
+
+Configuration FakeConfiguration()
+{
+    return new Configuration(
+        Faker.RandomNumber.Next(1000, 9999),
+        FakeParameters(Faker.RandomNumber.Next(2, 5))
+        );
+}
+
+Parameter FakeParameter()
+{
+    return new Parameter("Parameter_" + Faker.RandomNumber.Next(1000, 9999), "Value_" + Faker.RandomNumber.Next(1000, 9999));
+}
+
+List<Parameter> FakeParameters(int parameters)
+{
+    var result = new List<Parameter>();
+    for (int i = 0; i < parameters; i++)
+    {
+        result.Add(FakeParameter());
+    }
+    return result;
+}
+
+var users = new List<User>();
+for (int i = 0; i < 5; i++)
+{
+    users.Add(FakeUser());
+}
+
+var configurations = new List<Configuration>();
+for (int i = 0; i < 3; i++)
+{
+    configurations.Add(FakeConfiguration());
+}
+
+var context = new JsonContext(Path.Combine(Directory.GetCurrentDirectory(), "data"));
+context.InsertUpdateList(users);
+context.InsertUpdateList(configurations);
+context.InsertUpdate(new Car("DaciaSandero", "12345", 1));
+context.InsertUpdate(new Car("Toyota", "Yaris2", 43));
+context.Delete<Car>("Toyota");
+context.Delete<Car>("DaciaSandero");
+
+record User(int Id, string Name, string Email);
+
+record Configuration(int Id, List<Parameter> Parameters);
+
+record Parameter(string Key, string Value);
+
+record Car([property:JsonKey]string CarId, string Serial, int OwnerdId);
+
+
---
+++ JsonDb/JsonContext.cs
@@ -0,0 +1,135 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonDb;
+
+public class JsonContext
+{
+    protected string DataFolder { get; set; }
+
+    public JsonContext(string dataFolder)
+    {
+        DataFolder = dataFolder;
+        if(!Directory.Exists(DataFolder))
+        {
+            Directory.CreateDirectory(DataFolder);
+        }
+    }
+
+    protected static string NameOfFile<V>() => typeof(V).Name.ToLower() + ".json";
+    
+    public IEnumerable<V> Get<V>()
+    {
+        var file = Path.Combine(DataFolder, NameOfFile<V>());
+        if (!File.Exists(file))
+        {
+            File.Create(file).Close();
+            return Enumerable.Empty<V>();
+        }
+
+        return JsonConvert.DeserializeObject<IEnumerable<V>>(File.ReadAllText(file))
+            ?? Enumerable.Empty<V>();
+    }
+
+    public void InsertUpdateList<V>(IEnumerable<V> newValues)
+    {
+        var props = typeof(V).GetProperties();
+        var keyProperty = props.FirstOrDefault(p => p.GetCustomAttributes(typeof(JsonKey), false).Any());
+        if (keyProperty == null)
+        {
+            keyProperty = props.FirstOrDefault(p => p.Name == "Id");
+        }
+
+        if (keyProperty == null)
+        {
+            throw new JsonException($"Cannot identify key from type {nameof(V)}, use attribute {nameof(JsonKey)}");
+        }
+
+        var values = Get<V>().ToList();
+        foreach(var value in newValues)
+        {
+            var found = values.FirstOrDefault(v => keyProperty.GetValue(v)?.Equals(keyProperty.GetValue(value)) ?? false);
+            if (found != null)
+            {
+                found = value;
+            }
+            else
+            {
+                values.Add(value);
+            }
+        }
+
+        ApplyChanges(values);
+    }
+
+    public bool Delete<V>(object key)
+    {
+        var props = typeof(V).GetProperties();
+        var keyProperty = props.FirstOrDefault(p => p.GetCustomAttributes(typeof(JsonKey), false).Any());
+        if (keyProperty == null)
+        {
+            keyProperty = props.FirstOrDefault(p => p.Name == "Id");
+        }
+
+        if (keyProperty == null)
+        {
+            throw new JsonException($"Cannot identify key from type {nameof(V)}, use attribute {nameof(JsonKey)}");
+        }
+
+        var values = Get<V>();
+        var found = values.FirstOrDefault(v => keyProperty.GetValue(v)?.Equals(key) ?? false);
+        if (found == null)
+        {
+            return false;
+        }
+
+        var newValues = values.ToList();
+        var result = newValues.Remove(found);
+        ApplyChanges(newValues);
+        return result;
+    }
+
+    public void InsertUpdate<V>(V value)
+    {
+        var props = typeof(V).GetProperties();
+        var keyProperty = props.FirstOrDefault(p => p.GetCustomAttributes(typeof(JsonKey), false).Any());
+        if (keyProperty == null)
+        {
+            keyProperty = props.FirstOrDefault(p => p.Name == "Id");
+        }
+
+        if(keyProperty == null)
+        {
+            throw new JsonException($"Cannot identify key from type {nameof(V)}, use attribute {nameof(JsonKey)}");
+        }
+
+        var values = Get<V>();
+        var found = values.FirstOrDefault(v => keyProperty.GetValue(v)?.Equals(keyProperty.GetValue(value)) ?? false);
+        if (found != null)
+        {
+            found = value;
+            ApplyChanges(values);
+            return;
+        }
+
+        var newList = values.ToList();
+        newList.Add(value);
+        ApplyChanges(newList);
+    }
+
+    protected void ApplyChanges<V>(IEnumerable<V> values)
+    {
+        var file = Path.Combine(DataFolder, NameOfFile<V>());
+        if (!File.Exists(file))
+        {
+            File.Create(file).Close();
+        }
+
+        File.WriteAllText(file, JsonConvert.SerializeObject(values, Formatting.Indented));
+    }
+}
